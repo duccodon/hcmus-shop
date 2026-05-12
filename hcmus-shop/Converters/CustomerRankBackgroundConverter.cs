95ab19b9855b0609bf8201197a1dfa0e5ac661bc
Nguyen Thien Duc <ntduc22@clc.fitus.edu.vn>
2026-05-13 01:30:50 +0700

    fix table style

---
+++ hcmus-shop/Converters/CustomerRankBackgroundConverter.cs
@@ -0,0 +1,28 @@
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using System;
+
+namespace hcmus_shop.Converters
+{
+    public class CustomerRankBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var color = value?.ToString() switch
+            {
+                "Diamond" => ColorHelper.FromArgb(0xFF, 0xDB, 0xEA, 0xFE),
+                "Gold" => ColorHelper.FromArgb(0xFF, 0xFE, 0xF9, 0xC3),
+                "Silver" => ColorHelper.FromArgb(0xFF, 0xF3, 0xF4, 0xF6),
+                _ => ColorHelper.FromArgb(0xFF, 0xFE, 0xF3, 0xC7)
+            };
+
+            return new SolidColorBrush(color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ hcmus-shop/Converters/CustomerRankForegroundConverter.cs
@@ -0,0 +1,28 @@
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using System;
+
+namespace hcmus_shop.Converters
+{
+    public class CustomerRankForegroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var color = value?.ToString() switch
+            {
+                "Diamond" => ColorHelper.FromArgb(0xFF, 0x1D, 0x4E, 0x89),
+                "Gold" => ColorHelper.FromArgb(0xFF, 0x85, 0x4D, 0x0E),
+                "Silver" => ColorHelper.FromArgb(0xFF, 0x37, 0x41, 0x51),
+                _ => ColorHelper.FromArgb(0xFF, 0x92, 0x40, 0x0E)
+            };
+
+            return new SolidColorBrush(color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
